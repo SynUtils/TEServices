62403b26c1660a74ed10a22f2d2a1a52f978d5e4
Pavan Gupta <pavan.gupta@synerzip.com>
2014-04-14 11:32:48 +0530

    updated folder structure for download

--- c2c/models.py
+++ c2c/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.core.files import File
+from datetime import datetime
 
 
-
+date_time = datetime.now().strftime("%Y%m%d%H%M%S") 
 
 class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d/baseBuild', max_length=5234,blank=True, null=True,)
-    compare_build = models.FileField(upload_to='documents/%Y/%m/%d/compareBuild', max_length=5234,blank=True, null=True,)
-    
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d/' + date_time + '/baseBuild', max_length=5234,blank=True, null=True,)
+    compare_build = models.FileField(upload_to='documents/%Y/%m/%d/' + date_time + '/compareBuild', max_length=5234,blank=True, null=True,)
