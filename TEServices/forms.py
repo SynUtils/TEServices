4e2819b6297086b6cd899fa025ac10c7148da387
Pavan <pvn.gupta.2009@gmail.com>
2014-07-09 15:02:08 +0530

    Update forms.py

--- TEServices/forms.py
+++ TEServices/forms.py
@@ -7,6 +7,8 @@ class DocumentForm(forms.Form):
 
     docfile = forms.FileField(label='', show_hidden_initial='none',required=True,)
     compare_build = forms.FileField(label='', show_hidden_initial='none',required=True,)
+    test_data = forms.FileField(label='', show_hidden_initial='none',required=True,)
+
 
 class e2eForm(forms.Form):
     e2e_file = forms.FileField(label='', show_hidden_initial='none',required=True,)
